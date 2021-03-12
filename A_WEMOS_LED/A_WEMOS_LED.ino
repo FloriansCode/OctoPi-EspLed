e2d96baeb7c038f1e1ac1d178045b003a7e802ec
FloriansCode <70821651+FloriansCode@users.noreply.github.com>
2021-03-13 00:58:26 +0100

    Put all Code in one File

--- A_WEMOS_LED/A_WEMOS_LED.ino
+++ A_WEMOS_LED/A_WEMOS_LED.ino
@@ -83,3 +83,145 @@ WiFiClient client = server.available();   // Auf Clients (Server-Aufrufe) warten
 
       
 }
+
+
+//http request to get Job Information
+void API_JOB_CHECK (){
+    HTTPClient http;                        //Preparing the HTTP request.
+      http.begin(octoip_files);             //HTTP link
+      http.addHeader("X-Api-Key", APIKEY);  //add API KEY
+        int httpCode = http.GET();          //Send the request
+        String payload = http.getString();  //Get the request response payload
+      http.end();   //Close connection
+
+        // Just used: https://arduinojson.org/v6/assistant/ and striped that down
+        StaticJsonDocument<768> doc;
+        deserializeJson(doc, payload);      
+          JsonObject job = doc["job"];
+          JsonObject progress = doc["progress"];    
+            progress_completion = progress["completion"]; //Progressbar in %
+            String state = doc["state"]; // State of the Printer
+            String pre_state = "waiting";
+          //Serial Print status.
+          Serial.print("Status: ");
+          Serial.println(state);
+                    Serial.print("URL: ");
+          Serial.println(octoip_files);
+
+            if((state == "Operational") or (state == "Paused"))
+              {
+              if(pre_state != state){
+                  FadeOut();
+                }
+                
+              Operational();
+              pre_state= state;
+              }
+            else if(state == "Printing")
+              {
+              if(pre_state != state){
+                  FadeOut();
+                }
+                
+              Printing();
+              pre_state= state;
+              }
+            else if(state == "Cancelling")
+              {
+              if(pre_state != state){
+                  FadeOut();
+                }
+                
+              Cancelling();
+              pre_state= state;
+              }
+            else
+              {
+              if(pre_state != state){
+                  FadeOut();
+                }
+                
+              Offline();
+              pre_state= state;
+              }
+  }
+
+//different Printer states 
+void Operational()
+{
+          //Fade Out
+          strip.fill(strip.Color(LED_R,LED_G,LED_B), 0, 0);
+        for(LED_R, LED_G, LED_B; LED_R>=1,LED_G>=1, LED_B>=1 ; LED_R--,LED_G--,LED_B-- ){
+          strip.fill(strip.Color(LED_R,LED_G,LED_B), 0, 0);
+          strip.show();
+          delay(50);
+          }
+          //Reset Color //to be safe
+          LED_R = 0;
+          LED_G = 0;
+          LED_B = 0;
+
+         //Fade in
+         for(LED_R, LED_G, LED_B; LED_R<=50,LED_G<=50, LED_B<=50 ; LED_R++,LED_G++,LED_B++ ){
+          strip.fill(strip.Color(LED_R,LED_G,LED_B), 0, 0);
+          strip.show();
+          delay(50);
+          }
+          
+}
+void Printing()
+{
+        strip.show();
+        led_calc = (NUM_LEDS*  progress_completion) /100;
+        //Debug Progress
+        //Serial.print("Progress: "); 
+        //  Serial.println(progress_completion);
+        //Serial.print("Leds: ");
+        //  Serial.println(NUM_LEDS);
+        //Serial.print("Leds Clac: ");
+        //  Serial.println(led_calc);
+        
+        if(led_calc >=1){
+        strip.fill(strip.Color(50,50,50), 0, led_calc);
+        strip.show();
+        }
+        else{
+          strip.clear();
+          strip.fill(strip.Color(50,50,50), 0, 1);
+          strip.show();
+          }
+}
+void Cancelling(){
+          strip.fill(strip.Color(LED_R,LED_G,LED_B), 0, 0);
+          
+        for(LED_R; LED_R >=0; LED_R--){
+          strip.fill(strip.Color(LED_R,0,0), 0, 0);
+          strip.show();
+          Serial.println(LED_R);
+          delay(50);
+          }
+          LED_R = 0;
+          
+        for(LED_R; LED_R<=50; LED_R++){
+          strip.fill(strip.Color(LED_R,0,0), 0, 0);
+          strip.show();
+          Serial.println(LED_R);
+          delay(50);
+          }
+          LED_R = 50;
+}
+void Offline(){
+          strip.clear();
+          strip.show();
+}
+void FadeOut(){
+        for(LED_R, LED_G, LED_B; LED_R>=1,LED_G>=1, LED_B>=1 ; LED_R--,LED_G--,LED_B-- ){
+          strip.fill(strip.Color(LED_R,LED_G,LED_B), 0, 0);
+          strip.show();
+          delay(50);
+          }
+          //Reset Color
+          LED_R = 0;
+          LED_G = 0;
+          LED_B = 0;
+}
